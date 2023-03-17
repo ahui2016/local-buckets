e51c77866ed7d5f2ead84bc09300c06223bd10f0
ahui2016 <ahui.2015@icloud.com>
2023-03-17 23:26:36 +0800

    model.NewFile() 完成

--- model/model.go
+++ model/model.go
@@ -2,10 +2,14 @@ package model
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/ahui2016/local-buckets/util"
 	"github.com/samber/lo"
 )
 
@@ -93,10 +97,51 @@ type File struct {
 	Deleted  bool   `json:"deleted"`  // 把文件标记为 "已删除"
 }
 
-func NewFile(root, bucketID, name string) *File {
+func NewFile(root, bucketID, name string) (*File, error) {
+	now := Now()
+	info, err := os.Lstat(name)
+	if err != nil {
+		return nil, err
+	}
+	checksum, err := util.FileSum512(name)
+	if err != nil {
+		return nil, err
+	}
 	f := new(File)
-	return f
-	// TODO
+	f.Checksum = checksum
+	f.BucketID = bucketID
+	f.Name = name
+	f.Notes = ""
+	f.Keywords = ""
+	f.Size = info.Size()
+	f.Type = typeByFilename(name)
+	f.Like = 0
+	f.CTime = now
+	f.UTime = now
+	f.Checked = now
+	f.Damaged = false
+	f.Deleted = false
+	return f, nil
+}
+
+func Now() string {
+	return time.Now().Format(RFC3339)
+}
+
+func typeByFilename(filename string) (filetype string) {
+	ext := filepath.Ext(filename)
+	filetype = GetMIME(ext)
+	switch ext {
+	case "zip", "rar", "7z", "gz", "tar", "bz", "bz2", "xz":
+		filetype = "compressed/" + ext
+	case "md", "xml", "html", "xhtml", "htm", "yaml", "js", "ts", "go", "py", "cs", "dart", "rb", "c", "h", "cpp", "rs":
+		filetype = "text/" + ext
+	case "doc", "docx", "ppt", "pptx", "rtf", "xls", "xlsx":
+		filetype = "office/" + ext
+	case "epub", "pdf", "mobi", "azw", "azw3", "djvu":
+		filetype = "ebook/" + ext
+	}
+	return filetype
 }
 
 // FilesToString 把多个文件转换为多个文件的 ID 的字符串.
@@ -122,4 +167,139 @@ type CheckPwdForm struct {
 type ChangePwdForm struct {
 	OldPassword string `json:"old_password" validate:"required"`
 	NewPassword string `json:"new_password" validate:"required"`
+}
+
+// GetMIME returns the content-type of a file extension.
+// https://github.com/gofiber/fiber/blob/master/utils/http.go (edited).
+func GetMIME(extension string) (mime string) {
+	const MIMEOctetStream = "application/octet-stream"
+	extension = strings.ToLower(extension)
+
+	if len(extension) == 0 {
+		return
+	}
+	if extension[0] == '.' {
+		extension = extension[1:]
+	}
+	mime = mimeExtensions[extension]
+	if len(mime) == 0 {
+		return MIMEOctetStream
+	}
+	return mime
+}
+
+// MIME types were copied from
+// https://github.com/gofiber/fiber/blob/master/utils/http.go
+// https://github.com/nginx/nginx/blob/master/conf/mime.types
+var mimeExtensions = map[string]string{
+	"html":    "text/html",
+	"htm":     "text/html",
+	"shtml":   "text/html",
+	"css":     "text/css",
+	"xml":     "application/xml",
+	"gif":     "image/gif",
+	"jpeg":    "image/jpeg",
+	"jpg":     "image/jpeg",
+	"js":      "text/javascript",
+	"atom":    "application/atom+xml",
+	"rss":     "application/rss+xml",
+	"mml":     "text/mathml",
+	"txt":     "text/plain",
+	"jad":     "text/vnd.sun.j2me.app-descriptor",
+	"wml":     "text/vnd.wap.wml",
+	"htc":     "text/x-component",
+	"avif":    "image/avif",
+	"png":     "image/png",
+	"svg":     "image/svg+xml",
+	"svgz":    "image/svg+xml",
+	"tif":     "image/tiff",
+	"tiff":    "image/tiff",
+	"wbmp":    "image/vnd.wap.wbmp",
+	"webp":    "image/webp",
+	"ico":     "image/x-icon",
+	"jng":     "image/x-jng",
+	"bmp":     "image/x-ms-bmp",
+	"woff":    "font/woff",
+	"woff2":   "font/woff2",
+	"jar":     "application/java-archive",
+	"war":     "application/java-archive",
+	"ear":     "application/java-archive",
+	"json":    "application/json",
+	"hqx":     "application/mac-binhex40",
+	"doc":     "application/msword",
+	"pdf":     "application/pdf",
+	"ps":      "application/postscript",
+	"eps":     "application/postscript",
+	"ai":      "application/postscript",
+	"rtf":     "application/rtf",
+	"m3u8":    "application/vnd.apple.mpegurl",
+	"kml":     "application/vnd.google-earth.kml+xml",
+	"kmz":     "application/vnd.google-earth.kmz",
+	"xls":     "application/vnd.ms-excel",
+	"eot":     "application/vnd.ms-fontobject",
+	"ppt":     "application/vnd.ms-powerpoint",
+	"odg":     "application/vnd.oasis.opendocument.graphics",
+	"odp":     "application/vnd.oasis.opendocument.presentation",
+	"ods":     "application/vnd.oasis.opendocument.spreadsheet",
+	"odt":     "application/vnd.oasis.opendocument.text",
+	"pptx":    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+	"xlsx":    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+	"docx":    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	"wmlc":    "application/vnd.wap.wmlc",
+	"wasm":    "application/wasm",
+	"7z":      "application/x-7z-compressed",
+	"cco":     "application/x-cocoa",
+	"jardiff": "application/x-java-archive-diff",
+	"jnlp":    "application/x-java-jnlp-file",
+	"run":     "application/x-makeself",
+	"pl":      "application/x-perl",
+	"pm":      "application/x-perl",
+	"prc":     "application/x-pilot",
+	"pdb":     "application/x-pilot",
+	"rar":     "application/x-rar-compressed",
+	"rpm":     "application/x-redhat-package-manager",
+	"sea":     "application/x-sea",
+	"swf":     "application/x-shockwave-flash",
+	"sit":     "application/x-stuffit",
+	"tcl":     "application/x-tcl",
+	"tk":      "application/x-tcl",
+	"der":     "application/x-x509-ca-cert",
+	"pem":     "application/x-x509-ca-cert",
+	"crt":     "application/x-x509-ca-cert",
+	"xpi":     "application/x-xpinstall",
+	"xhtml":   "application/xhtml+xml",
+	"xspf":    "application/xspf+xml",
+	"zip":     "application/zip",
+	"bin":     "application/octet-stream",
+	"exe":     "application/octet-stream",
+	"dll":     "application/octet-stream",
+	"deb":     "application/octet-stream",
+	"dmg":     "application/octet-stream",
+	"iso":     "application/octet-stream",
+	"img":     "application/octet-stream",
+	"msi":     "application/octet-stream",
+	"msp":     "application/octet-stream",
+	"msm":     "application/octet-stream",
+	"mid":     "audio/midi",
+	"midi":    "audio/midi",
+	"kar":     "audio/midi",
+	"mp3":     "audio/mpeg",
+	"ogg":     "audio/ogg",
+	"m4a":     "audio/x-m4a",
+	"ra":      "audio/x-realaudio",
+	"3gpp":    "video/3gpp",
+	"3gp":     "video/3gpp",
+	"ts":      "video/mp2t",
+	"mp4":     "video/mp4",
+	"mpeg":    "video/mpeg",
+	"mpg":     "video/mpeg",
+	"mov":     "video/quicktime",
+	"webm":    "video/webm",
+	"flv":     "video/x-flv",
+	"m4v":     "video/x-m4v",
+	"mng":     "video/x-mng",
+	"asx":     "video/x-ms-asf",
+	"asf":     "video/x-ms-asf",
+	"wmv":     "video/x-ms-wmv",
+	"avi":     "video/x-msvideo",
 }
