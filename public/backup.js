16a8cf38b37dc3cad8671b95554f218e7b67c93b
ahui2016 <ahui.2015@icloud.com>
2023-03-26 21:24:01 +0800

    BKProjList

--- public/backup.js
+++ public/backup.js
@@ -30,7 +30,7 @@ const CreateBKProjForm = cc("form", {
   children: [
     MJBS.createFormControl(
       BKProjPathInput,
-      "Backup Project",
+      "新建備份專案",
       "備份專案的絕對路徑, 必須是一個空資料夾."
     ),
     MJBS.hiddenButtonElem(),
@@ -51,26 +51,112 @@ const CreateBKProjForm = cc("form", {
         body: { text: bkProjectPath },
         alert: BKProjCreateAlert,
         onSuccess: () => {
+          BKProjCreateBtn.hide();
           BKProjCreateAlert.clear().insert("success", "創建備份專案, 成功!");
+          BKProjCreateAlert.insert("info", "三秒後將會自動刷新本頁");
+          setTimeout(() => {
+            window.location.reload();
+          }, 5000);
         },
         onAlways: () => {
           MJBS.enable(BKProjCreateBtn); // ------------------- enable
+          MJBS.focus(BKProjPathInput);
         },
       });
     }),
   ],
 });
 
+const BKProjList = cc("div", { classes: "accordion" });
+
+function BKProjItem(projPath) {
+  const UseBtn = MJBS.createButton("use");
+  const DelBtn = MJBS.createButton("delete", "secondary");
+  const DangerDelBtn = MJBS.createButton("DELETE", "danger");
+  const ItemAlert = MJBS.createAlert();
+
+  const body = cc("div", {
+    classes: "accordion-collapse collapse",
+    attr: { "data-bs-parent": BKProjList.id },
+    children: [
+      m("div")
+        .addClass("accordion-body")
+        .append(
+          m("div")
+            .addClass("text-end mb-2")
+            .append(
+              m(UseBtn).addClass("me-2"),
+              m(DelBtn).on("click", (event) => {
+                event.preventDefault();
+                ItemAlert.insert(
+                  "warning",
+                  "待 delete 按鈕變紅色後再點擊一次, 執行刪除."
+                );
+                setTimeout(() => {
+                  DelBtn.hide();
+                  DangerDelBtn.show();
+                }, 5000);
+              }),
+              m(DangerDelBtn).hide()
+            ),
+          m(ItemAlert)
+        ),
+    ],
+  });
+
+  return cc("div", {
+    classes: "accordion-item",
+    children: [
+      m("h2")
+        .addClass("accordion-header")
+        .append(
+          m("button")
+            .addClass("accordion-button collapsed")
+            .text(projPath)
+            .attr({
+              type: "button",
+              "data-bs-toggle": "collapse",
+              "data-bs-target": body.id,
+              "aria-expanded": false,
+              "aria-controls": body.raw_id,
+            })
+        ),
+      m(body),
+    ],
+  });
+}
+
 $("#root")
   .css(RootCss)
   .append(
     navBar.addClass("my-3"),
     m(PageAlert).addClass("my-5"),
-    m(CreateBKProjForm).addClass("my-5")
+    m(PageLoading).addClass("my-5"),
+    m(CreateBKProjForm).addClass("my-5").hide(),
+    m(BKProjList).addClass("my-5").hide()
   );
 
 init();
 
 function init() {
+  getBKProjects();
   MJBS.focus(BKProjPathInput);
+}
+
+function getBKProjects() {
+  axiosGet({
+    url: "/api/project-config",
+    alert: PageAlert,
+    onSuccess: (resp) => {
+      const bkProjects = resp.data.backup_projects;
+      if (bkProjects && bkProjects.length > 0) {
+        BKProjList.show();
+        MJBS.appendToList(BKProjList, bkProjects.map(BKProjItem));
+      }
+    },
+    onAlways: () => {
+      PageLoading.hide();
+      CreateBKProjForm.show();
+    },
+  });
 }
